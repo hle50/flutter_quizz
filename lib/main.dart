f2f64872f2fe4e7e8923bdcfd4c8942f5c9d2011
hoa <hoa@lodestonesolutions.com.au>
2021-07-22 13:49:47 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quizz/screens/home.dart';
+
+import 'helpers/database_helper.dart';
+import 'models/grocery.dart';
+
+void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key? key, required this.title}) : super(key: key);
+
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+
+//   final String title;
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int? selectedId;
+//   final textController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // This method is rerun every time setState is called, for instance as done
+//     // by the _incrementCounter method above.
+//     //
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: TextField(
+//           controller: textController,
+//         ),
+//       ),
+//       body: Center(
+//         child: FutureBuilder<List<Grocery>>(
+//             future: DatabaseHelper.instance.getGroceries(),
+//             builder:
+//                 (BuildContext context, AsyncSnapshot<List<Grocery>> snapshot) {
+//               if (!snapshot.hasData) {
+//                 return CircularProgressIndicator();
+//               }
+//               return snapshot.data!.isEmpty
+//                   ? Center(child: Text('No Groceries in List.'))
+//                   : ListView(
+//                       children: snapshot.data!.map((grocery) {
+//                         return Center(
+//                           child: Card(
+//                             color: selectedId == grocery.id
+//                                 ? Colors.white70
+//                                 : Colors.white,
+//                             child: ListTile(
+//                               title: Text(grocery.name),
+//                               onTap: () {
+//                                 setState(() {
+//                                   if (selectedId == null) {
+//                                     textController.text = grocery.name;
+//                                     selectedId = grocery.id;
+//                                   } else {
+//                                     textController.text = '';
+//                                     selectedId = null;
+//                                   }
+//                                 });
+//                               },
+//                               onLongPress: () {
+//                                 setState(() {
+//                                   DatabaseHelper.instance.remove(grocery.id!);
+//                                 });
+//                               },
+//                             ),
+//                           ),
+//                         );
+//                       }).toList(),
+//                     );
+//             }),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.save),
+//         onPressed: () async {
+//           selectedId != null
+//               ? await DatabaseHelper.instance.update(
+//                   Grocery(id: selectedId, name: textController.text),
+//                 )
+//               : await DatabaseHelper.instance.add(
+//                   Grocery(name: textController.text),
+//                 );
+//           setState(() {
+//             textController.clear();
+//             selectedId = null;
+//           });
+//         },
+//       ),
+//       // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
---
+++ lib/screens/home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff0288D1),
+      body: Center(
+        child: GridView.count(
+          shrinkWrap: true,
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.orange,
+                  border: Border.all(
+                      color: Colors.white, // Set border color
+                      width: 1.0), // Set border width
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(5.0)), // Set rounded corner radius
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      color: Colors.grey,
+                      // offset: Offset(1, 3),
+                    )
+                  ] // Make rounded corner of border
+                  ),
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.child_care,
+                    color: Colors.pink,
+                    size: 44.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                ],
+              ),
+              // color: Colors.teal[100],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Heed not the rabble'),
+              color: Colors.teal[200],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Sound of screams but the'),
+              color: Colors.teal[300],
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Who scream'),
+              color: Colors.teal[400],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
